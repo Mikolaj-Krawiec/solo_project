1e82a8f21550624e1400276cc015891bf4b11787
Mikolaj-Krawiec <59422679+Mikolaj-Krawiec@users.noreply.github.com>
2020-05-06 18:43:02 +0200

    Add pop-up chat

--- src/js/script.js
+++ src/js/script.js
@@ -1,4 +1,6 @@
 /* eslint-disable linebreak-style */
+/* eslint-disable no-unused-vars */
+
 {
   const initNavSlide = () => {
     const burger = document.querySelector('.burger');
@@ -29,24 +31,23 @@
   const pages = document.querySelectorAll('.page');
 
   const initPages = () => {
-    const idFromHash = window.location.hash.replace('#/','');
-    
+    const idFromHash = window.location.hash.replace('#/', '');
 
     let pageMatchingHash = pages[0].id;
 
-    for(let page of pages) {
-      if(page.id == idFromHash) {
+    for (let page of pages) {
+      if (page.id == idFromHash) {
         pageMatchingHash = page.id;
         break;
       }
     }
 
     activatePage(pageMatchingHash);
-    
+
     for (const link of navLinks) {
       link.addEventListener('click', (event) => {
         event.preventDefault();
-        const id = event.currentTarget.getAttribute('href').replace('#','');
+        const id = event.currentTarget.getAttribute('href').replace('#', '');
         activatePage(id);
         window.location.hash = '#/' + id;
       });
@@ -54,48 +55,116 @@
   };
 
   const activatePage = (pageId) => {
-    for(let page of pages) {
-      page.classList.toggle('page-active' , page.id == pageId);
+    for (let page of pages) {
+      page.classList.toggle('page-active', page.id == pageId);
     }
-    for(let link of navLinks) {
-      link.classList.toggle('link-active', link.getAttribute('href') == '#' + pageId );
+    for (let link of navLinks) {
+      link.classList.toggle(
+        'link-active',
+        link.getAttribute('href') == '#' + pageId
+      );
     }
   };
 
+  const overlay = document.querySelector('#overlay');
+  const popUps = document.querySelectorAll('.pop-up');
+  const popUpsOpen = document.querySelectorAll('.js-pop-up-open');
+  const popUpsClose = document.querySelectorAll('.js-pop-up-close');
+
   const initPopUps = () => {
-    const popUpTriggers = document.querySelectorAll('.pop-up-message');
-    const popUps = document.querySelectorAll('.pop-up');
-    const popUpsButtons = document.querySelectorAll('.pop-btn');
-    const overlay = document.querySelector('.overlay');
-    
-    for(const button of popUpsButtons) {
-      button.addEventListener('click', event => {
-        event.preventDefault();
-        for(const popUp of popUps) {
-          popUp.classList.remove('active');
-        }
-        overlay.classList.remove('active');
-      });
+    for (const button of popUpsClose) {
+      button.addEventListener('click', closePopUps);
     }
 
-    for(const button of popUpTriggers) {
-      button.addEventListener('click', event => {
+    overlay.addEventListener('click', closePopUps);
+
+    document.addEventListener('keyup', (e) => {
+      if (e.keyCode === 27) {
+        closePopUps(event);
+      }
+    });
+
+    for (const button of popUpsOpen) {
+      button.addEventListener('click', (event) => {
         event.preventDefault();
-        const id = event.currentTarget.getAttribute('href').replace('#','');
-        for(const popUp of popUps) {
+        const id = event.currentTarget.getAttribute('href').replace('#', '');
+        for (const popUp of popUps) {
           popUp.classList.toggle('active', popUp.id === id);
         }
         overlay.classList.toggle('active');
-      });  
+      });
     }
 
-    
+    const chatSend = document.querySelector('#chat-send');
+    const chatInput = document.querySelector('#chat-input');
+    const chatBox = document.querySelector('#chat-box');
+
+    chatSend.addEventListener('click', (event) => {
+      event.preventDefault();
+      if (chatInput.value) {
+        const question = document.createElement('div');
+        question.classList.add('question');
+        question.innerHTML = chatInput.value;
+
+        chatBox.insertAdjacentElement('beforeEnd', question);
+        chatInput.value = '';
+      }
+    });
+  };
+
+  const closePopUps = (event) => {
+    event.preventDefault();
+    for (const popUp of popUps) {
+      popUp.classList.remove('active');
+    }
+    overlay.classList.remove('active');
+  };
+
+  const initChart = () => {
+    const ctx = document.getElementById('myChart').getContext('2d');
+
+    // eslint-disable-next-line no-undef
+    const chart = new Chart(ctx, {
+      // 1
+      type: 'bar',
+      data: {
+        // 2
+        labels: ['01', '02', '03', '04', '05', '06', '07', '08', '09', '10'],
+        // 3
+        datasets: [
+          {
+            // 4
+            label: 'Signups',
+            // 5
+            backgroundColor: '#8DBEC8',
+            borderColor: '#8DBEC8',
+            // 6
+            data: [52, 51, 41, 94, 26, 6, 72, 9, 21, 88],
+          },
+          {
+            label: 'FTD',
+            backgroundColor: '#F29E4E',
+            borderColor: '#F29E4E',
+            data: [6, 72, 1, 0, 47, 11, 50, 44, 63, 76],
+          },
+          {
+            label: 'Earned',
+            backgroundColor: '#71B374',
+            borderColor: '#71B374',
+            data: [59, 49, 68, 90, 67, 41, 13, 38, 48, 48],
+            // 7
+            hidden: true,
+          },
+        ],
+      },
+    });
   };
 
   const app = () => {
     initPages();
     initNavSlide();
     initPopUps();
+    initChart();
   };
 
   app();
